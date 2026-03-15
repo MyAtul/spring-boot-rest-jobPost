c0559d6a073a16c8448bf16e3d6feb8a535d5692
atul <atuly2409@gmail.com>
2026-03-15 10:34:09 +0530

    Remove the credential

--- src/main/java/com/telusko/spring_boot_rest/repo/JobRepo.java
+++ src/main/java/com/telusko/spring_boot_rest/repo/JobRepo.java
@@ -5,8 +5,7 @@ import com.telusko.spring_boot_rest.model.JobPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 
 @Repository
